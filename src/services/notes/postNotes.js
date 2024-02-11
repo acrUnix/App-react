8aa58bafb1a2fab4e7756b9cef9c8a5728dc54d8
acrUnix <angelramirez.unix@gmail.com>
2024-02-11 13:59:56 -0300

    modification on client

--- src/services/notes/postNotes.js
+++ src/services/notes/postNotes.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-export const postNotes = (newNts)=>{
-    return axios.post('http://localhost:3001/api/notes', newNts)
+export const postNotes = async (newNts)=>{
+    return await axios.post('http://localhost:3001/api/notes', newNts)
     .then((response)=>{
         const newNt = response.data
         return newNt
